47a46bcbf2117e6c2bbddb12a88c37460535c3c7
kyrbuk <sashakorsyn@gmail.com>
2024-10-30 15:25:55 +0200

    Shows address for conn

--- SeaBattle/Server.cs
+++ SeaBattle/Server.cs
@@ -15,6 +15,7 @@ public class Server
     public Server(int port)
     {
         _server = new TcpListener(IPAddress.Any, port);
+        Console.WriteLine(Dns.GetHostName() + ":" + port);
     }
 
     public void Start()
